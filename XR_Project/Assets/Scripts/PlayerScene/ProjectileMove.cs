9336e6756fd683d00ce49ed18a1c353e56109fe4
sangbeen0912 <sangbeen0912@gmail.com>
2023-04-26 21:08:47 +0900

    Dotween

--- XR_Project/Assets/Scripts/PlayerScene/ProjectileMove.cs
+++ XR_Project/Assets/Scripts/PlayerScene/ProjectileMove.cs
@@ -40,12 +40,16 @@ public class ProjectileMove : MonoBehaviour
         {
             Destroy(this.gameObject);
             other.gameObject.GetComponent<Monster>().Damaged(1);
+            GameObject Temp = GameObject.FindGameObjectWithTag("GameManager");
+            Temp.GetComponent<HUDTextManager>().UpdateHUDTextSet("1", other.gameObject, new Vector3(0.0f, 10.0f, 0.0f));
         }
 
-        if (other.CompareTag("Monster") && projectileType == PROJECTILETYPE.MONSTER)    //tag를 검사한다
+        if (other.CompareTag("Player") && projectileType == PROJECTILETYPE.MONSTER)    //tag를 검사한다
         {
             Destroy(this.gameObject);
             other.gameObject.GetComponent<PlayerHp>().Damaged(1);
+            GameObject Temp = GameObject.FindGameObjectWithTag("GameManager");
+            Temp.GetComponent<HUDTextManager>().UpdateHUDTextSet("1", other.gameObject, new Vector3(0.0f, 10.0f, 0.0f));
         }
     }
 }
